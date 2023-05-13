63df2ddb7fcaee510961f5c3508f8fc3c34d6cc6
ِAhmed Helmy <125694803+ahmdhlm@users.noreply.github.com>
2023-05-13 13:08:01 +0300

    uart.c

--- SRC/MCAL/uart.c
+++ SRC/MCAL/uart.c
@@ -1,17 +1,30 @@
-#include "uart.h"
+/**********************************
+*	Auther :Ahmed Gamal Helmy
+*	Date: 5/5/2023
+*	filename: uart.c
+***********************************/
+
+
+#include "..\..\SERVICES\TM4C123.h"
+#include "..\..\SERVICES\Bit_Utilies.h"
+#include "../../INCLUDE/MCAL/uart.h"
 
 
 void UART0_init(uint16  baudRate)
 {
-	double x=CLOCK_RATE/(16.0*baudRate);  /************  configure BD RATE  ********************/
-	SET_BIT(SYSCTL_RCGCUART_R,0);    							/*************  set pin 0 to provide clock ***********/
-	SET_BIT(SYSCTL_RCGCGPIO_R,0) ;                /**************   Enable and provide a clock to GPIO Port A in Run mode   ***************/
-	while((SYSCTL_PRGPIO_R & 0x00000001) == 0){}  	   /***********   wait for GPIO getting ready  *************/
-	CLEAR_BIT(UART0_CTL_R,0);                     /******* disable UART to configure baud rate ***********/
-		UART0_IBRD_R=(int)x;                        /******integer reg to store integer value of baudrate **********/
-		UART0_FBRD_R=(int) ((x-UART0_IBRD_R)*64+0.5 );   /*********** float reg to store the fraction of baudrate **********/
-		UART0_LCRH_R=0x00000070;                       /***********       8bit, no parity bits,one stop, fifo   *********/
-		UART0_CTL_R |= 0x00000301;   										 /*************** enable UART,TX,RX  *************/
+	SET_BIT(SYSCTL_RCGCUART_R,0); 	                 /*******  set pin 0 to provide clock ***********/
+	while((SYSCTL_PRUART_R & 0x00000001) == 0){}
+	SET_BIT(SYSCTL_RCGCGPIO_R,0) ;                   /********   Enable and provide a clock to GPIO Port A in Run mode   *********/
+	while((SYSCTL_PRGPIO_R & 0x00000001) == 0){}  	 /*******   wait for GPIO getting ready  ********/
+		
+		//GPIO_PORTA_LOCK_R |= GPIO_LOCK_KEY;    // 2) unlock GPIO Port F
+		//GPIO_PORTA_CR_R |= 0xFF;               // allow changes to PF4-0
+		
+		CLEAR_BIT(UART0_CTL_R,0);                   /******* disable UART to configure baud rate *************/
+		UART0_IBRD_R= 104 ;                        /******** integer reg to store integer value of baudrate **/
+		UART0_FBRD_R= 11 ;                        /********* float reg to store the fraction of baudrate *****/
+		UART0_LCRH_R=0x00000070;                 /********** 8bit, no parity bits,one stop, fifo   ***********/
+		UART0_CTL_R |= 0x00000301;   					 /************ enable UART,TX,RX  ******************************/
 		
 		/*********   digital enable  **************/
 		SET_BIT(GPIO_PORTA_DEN_R,0);
@@ -36,15 +49,16 @@ void UART0_init(uint16  baudRate)
 /********************  RX---->PD6  ********   TX-------->PD7  **************************/
 void UART2_init(uint16  baudRate)
 {
-	double x=CLOCK_RATE/(16.0*baudRate);  /************  configure BD RATE  ********************/
-	SET_BIT(SYSCTL_RCGCUART_R,2);    							/*************  set pin 2 to provide clock ***********/
-	SET_BIT(SYSCTL_RCGCGPIO_R,3) ;                /**************   Enable and provide a clock to GPIO Port D in Run mode   ***************/
-	while((SYSCTL_PRGPIO_R & (0x01<<3)) == 0){}  	   /***********   wait for GPIO portD getting ready  *************/
-	CLEAR_BIT(UART2_CTL_R,0);                     /******* disable UART2 to configure baud rate ***********/
-		UART2_IBRD_R=(int)x;                        /******integer reg to store integer value of baudrate **********/
-		UART2_FBRD_R=(int) ((x-UART0_IBRD_R)*64+0.5 ) ;   /*********** float reg to store the fraction of baudrate **********/
-		UART2_LCRH_R=0x00000070;                       /***********       8bit, no parity bits,one stop, fifo   *********/
-		UART2_CTL_R |= 0x00000301;   										 /*************** enable UART,TX,RX  *************/
+	SET_BIT(SYSCTL_RCGCUART_R,2);    							/*************  set pin 2 to provide clock ***********************************/
+	SET_BIT(SYSCTL_RCGCGPIO_R,3) ;                /*************  Enable and provide a clock to GPIO Port D in Run mode   *****/
+	while((SYSCTL_PRGPIO_R & (0x01<<3)) == 0){}  	/*********** *  wait for GPIO portD getting ready  *************************/
+	  GPIO_PORTD_LOCK_R |= GPIO_LOCK_KEY;    // 2) unlock GPIO Port F
+		GPIO_PORTD_CR_R |= 0xFF;               // allow changes to PF4-0
+		CLEAR_BIT(UART2_CTL_R,0);                /******* disable UART2 to configure baud rate *************/
+		UART2_IBRD_R=104;                        /******* integer reg to store integer value of baudrate **/
+		UART2_FBRD_R=11 ;                        /******* float reg to store the fraction of baudrate ****/
+		UART2_LCRH_R=0x00000070;                 /******* 8bit, no parity bits,one stop, fifo   *********/
+		UART2_CTL_R |= 0x00000301;   						 /******* enable UART,TX,RX  ***************************/
 		
 		/*********   digital enable  **************/
 		SET_BIT(GPIO_PORTD_DEN_R,6);
@@ -68,16 +82,16 @@ void UART2_init(uint16  baudRate)
 
 
 /**********   function to recieve byte UART0******/
-uint8 UART0_recieveByte(void){
+char UART0_recieveByte(void){
 	while (     BIT_IS_SET(UART0_FR_R,4)  ){}  /**********  check until RXFE =0  **************/
-		return (uint8)UART0_DR_R&0xFF;   /********      return the value    ****************/
+		return (uint8)UART0_DR_R&0xFF;          /********      return the value    **************/
 		
 }
 
 /**********   function to recieve byte UART2****/
-uint8 UART2_recieveByte(void){
+char UART2_recieveByte(void){
 	while (  BIT_IS_SET(UART2_FR_R,4)   ){}  /**********  check until RXFE =0  **************/
-		return (uint8)UART2_DR_R&0xFF;   /********      return the value    ****************/
+		return (uint8)UART2_DR_R&0xFF;        /*********    return the value    **************/
 		
 }
 
@@ -87,18 +101,21 @@ uint8 UART2_recieveByte(void){
 
 
 /**********   function to recieve string UART0***********/
-void UART0_receiveString(uint8 *Str)
+void UART0_receiveString(char *Str , char stopChar)
 {
+
 	uint8 i = 0;
 
 	/* Receive the first byte */
 	Str[i] =  UART0_recieveByte();
-
+	UART0_sendByte(Str[i]);
 	/* Receive the whole string until the '#' */
-	while(Str[i] != '#')
+	while(Str[i] != stopChar)
 	{
 		i++;
 		Str[i] =  UART0_recieveByte();
+		if(Str[i]!=',')
+		UART0_sendByte(Str[i]);
 	}
 
 	/* After receiving the whole string plus the '#', replace the '#' with '\0' */
@@ -108,7 +125,7 @@ void UART0_receiveString(uint8 *Str)
 
 
 /**********   function to recieve string UART2***********/
-void UART2_receiveString(uint8 *Str)
+void UART2_receiveString(char *Str , char stopChar)
 {
 	uint8 i = 0;
 
@@ -116,7 +133,7 @@ void UART2_receiveString(uint8 *Str)
 	Str[i] =  UART2_recieveByte();
 
 	/* Receive the whole string until the '#' */
-	while(Str[i] != '#')
+	while(Str[i] != stopChar)
 	{
 		i++;
 		Str[i] =  UART2_recieveByte();
@@ -140,4 +157,12 @@ void UART2_sendByte(uint8 data)
 {
 	while(BIT_IS_SET(UART2_FR_R,5));	/**********  check until TXFF =0  **************/
 	UART2_DR_R=data;
+}
+
+/*************** Send String *************/
+void UART_sendString(char * ptr){
+while(*ptr != NULL){
+	UART0_sendByte(*ptr);
+	ptr++;
+}
 }
