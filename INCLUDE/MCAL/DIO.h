5e34ab91ab50f5fc9be8cda7961912566b4c7aca
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2023-05-13 13:57:09 +0300

    Add files via upload

---
+++ INCLUDE/MCAL/DIO.h
@@ -0,0 +1,20 @@
+
+/* Name : Yousif Hazim Nazar */
+/* Code : 2001741 */
+
+
+#ifndef DIO_H_
+#define DIO_H_
+
+#include "../../SERVICES/Bit_Utilies.h"
+
+void GPIO_initPortA();
+void GPIO_initPortB();
+void GPIO_initPortC();
+void GPIO_initPortD();
+void GPIO_initPortE();
+void GPIO_initPortF();
+
+
+
+#endif /* DIO_H_ */
